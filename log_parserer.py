0a95c467131e4ff428e8105323acd484247c2b2c
iamnageshdevadiga <iamnageshdevadiga@gmail.com>
2023-11-14 22:40:35 +0530

    negative test case added

--- log_parserer.py
+++ log_parserer.py
@@ -2,6 +2,33 @@
 
 import json
 
+def check_string_contains_key_value_pairs(input_string, delimiter=' '):
+    """
+    Check if the given string contains key and value pairs.
+
+    Parameters:
+    - input_string (str): The input string to check.
+    - delimiter (str, optional): The delimiter between key-value pairs.
+
+    Returns:
+    - bool: True if key-value pairs are found, False otherwise.
+
+    Example:
+     # check_string_contains_key_value_pairs("data=obj1 a=b")
+        True
+     #check_string_contains_key_value_pairs("This is a invalid string not contained any key value pair.")
+        False
+    """
+    key_value_pairs = input_string.split(delimiter)
+    
+    for pair in key_value_pairs:
+        if '=' in pair:
+            # if at least one key-value pair is found then  return True
+            return True
+
+    # if no key-value pair is found then return False
+    return False
+
 def processed_string(input_string):
     """
     Parse a set of key-value pairs from an input string and store them in a dictionary.
@@ -18,7 +45,10 @@ def processed_string(input_string):
     {'cat': 'C2', 'cs1Label': 'subcat', 'cs1': 'DNS_TUNNELING', 'msg': 'Malicious activity', 'dhost': 'bad.com'}
     
     """
+    
     parsed_data = {}
+    if not check_string_contains_key_value_pairs(input_string):
+        return parsed_data
     key_list = ["cat", "cs1", "cs2Label","cs1Label", "cs2", "cs3Label", "cs3", "cs4Label", "cs4", "cn1Label", "cn1", "msg", "dhost", "dst"]
     
     for key in key_list:
@@ -45,18 +75,20 @@ def parse_key_value(input_string,key, end=" "):
     'bad.com'
     
     """
-    if key == "msg":
-        initial_index = input_string.find(key  + "=")
-        value = input_string[initial_index + len(key) + 1:].split('.')[0]
-        return ' '.join(value.split(end, 1))
     
-    elif key+'=' in input_string:
+    if key+'=' in input_string:
+        if key == "msg":
+            initial_index = input_string.find(key  + "=")
+            value = input_string[initial_index + len(key) + 1:].split('.')[0]
+            return ' '.join(value.split(end, 1))
+        
         initial_index = input_string.find(key+"=")
         final_index = input_string[initial_index:].find(end)
         final_string = input_string[initial_index:final_index+initial_index]
         return final_string.split("=",1)[1]
+    
     else:
-        return 'None'
+        return "None"
 
 if __name__ == "__main__":
     system_log_string = "SAC:0|Sacumen|CAAS|2021.2.0|3|MALICIOUS|High|cat=C2 cs1Label=subcat cs1=DNS_TUNNELING cs2Label=vueUrls cs2=https://aws-dev.sacdev.io/alerts?filter=alertId%3D%3D81650 cs3Label=Tags cs3=USA,Finance cs4Label=Url cs4=https://aws-dev.sacdev.io/settings/tir?rules.sort=4%3A1&filter=state%3D%3D2&selected=9739323 cn1Label=severityScore cn1=900 msg=Malicious activity was reported in CAAS\= A threat intelligence rule has been automatically created in DAAS. dhost=bad.com dst=1.1.1.1"
--- test_log_parserer.py
+++ test_log_parserer.py
@@ -5,8 +5,7 @@ def test_string_parserer():
     """
         Test function for the 'parsed_string' method in the log_parserer module.
         It checks if the 'parsed_string' method correctly parses a given system log string and returns the expected result.
-        Example:
-        >>> test_parsed_string()
+        
     """
     # Input system log string
     input_string = "SAC:0|Sacumen|CAAS|2021.2.0|3|MALICIOUS|High|cat=C2 cs1Label=subcat cs1=DNS_TUNNELING cs2Label=vueUrls cs2=https://aws-dev.sacdev.io/alerts?filter=alertId%3D%3D81650 cs3Label=Tags cs3=USA,Finance cs4Label=Url cs4=https://aws-dev.sacdev.io/settings/tir?rules.sort=4%3A1&filter=state%3D%3D2&selected=9739323 cn1Label=severityScore cn1=900 msg=Malicious activity was reported in CAAS\= A threat intelligence rule has been automatically created in DAAS. dhost=bad.com dst=1.1.1.1"
@@ -31,6 +30,65 @@ def test_string_parserer():
             }
     
     #parsed string
+    result = processed_string(input_string)
+    
+    # Assertion to check if the result matches the expected result
+    assert result == expected_output
+
+def test_invalid_input_string():
+    """
+    Test case to check the behavior of the 'processed_string' function when provided with an invalid input string.
+
+    Input:
+    - Invalid string without key and value pairs.
+
+    Expected Output:
+    - An empty dictionary since the input string doesn't contain any key andvalue pairs.
+    """
+    input_string = "Invalid string without key and value pairs"
+    
+    # Dictionary contains empty data
+    expected_output = {}
+
+    #parsed string 
+    result = processed_string(input_string)
+    
+    # Assertion to check if the result matches the expected result
+    assert result == expected_output
+
+def test_valid_input_string_with_missing_key_pair():
+    """
+    Test case to check the behavior of the 'processed_string' function when a valid input string is missing a key-value pair.
+
+    Input:
+    - Input system log string doesn't contain "cs1Label=subcat".
+
+    Expected Output:
+    - Dictionary with the parsed key and value pairs. The missing key, in this case, "cs1Label", should have the value "None".
+    """
+    # Input system log string
+    input_string = "SAC:0|Sacumen|CAAS|2021.2.0|3|MALICIOUS|High|cat=C2 cs1=DNS_TUNNELING cs2Label=vueUrls cs2=https://aws-dev.sacdev.io/alerts?filter=alertId%3D%3D81650 cs3Label=Tags cs3=USA,Finance cs4Label=Url cs4=https://aws-dev.sacdev.io/settings/tir?rules.sort=4%3A1&filter=state%3D%3D2&selected=9739323 cn1Label=severityScore cn1=900 msg=Malicious activity was reported in CAAS\= A threat intelligence rule has been automatically created in DAAS. dhost=bad.com dst=1.1.1.1"
+    input_string += ' '
+    
+    # Dictionary contains empty data
+    expected_output = {
+            "cat": "C2",
+            "cs1": "DNS_TUNNELING",
+            "cs2Label": "vueUrls",
+            "cs1Label": "None",
+            "cs2": "https://aws-dev.sacdev.io/alerts?filter=alertId%3D%3D81650",
+            "cs3Label": "Tags",
+            "cs3": "USA,Finance",
+            "cs4Label": "Url",
+            "cs4": "https://aws-dev.sacdev.io/settings/tir?rules.sort=4%3A1&filter=state%3D%3D2&selected=9739323",
+            "cn1Label": "severityScore",
+            "cn1": "900",
+            "msg": "Malicious activity was reported in CAAS\\= A threat intelligence rule has been automatically created in DAAS",
+            "dhost": "bad.com",
+            "dst": "1.1.1.1"
+        }
+
+    #parsed string 
     result = processed_string(input_string)
     
     # Assertion to check if the result matches the expected result
