9d780623019188dfb45aff738d6c11ad0a3a875a
iamnageshdevadiga <iamnageshdevadiga@gmail.com>
2023-11-14 17:56:27 +0530

    code refactor and documentation added

--- log_parserer.py
+++ log_parserer.py
@@ -1,27 +1,65 @@
+#This Python script designed to parse antivirus system log strings and extract key information..
+
 import json
 
-def parsed_string(input_string):
+def processed_string(input_string):
+    """
+    Parse a set of key-value pairs from an input string and store them in a dictionary.
+
+    Parameters:
+    - input_string (str): The input string containing key-value pairs.
+
+    Returns:
+    - dict: A dictionary containing parsed key-value pairs based on the provided key list.
+
+    Example:
+    >>> input_str = "cat=C2 cs1Label=subcat cs1=DNS_TUNNELING msg=Malicious activity dhost=bad.com"
+    >>> parsed_string(input_str)
+    {'cat': 'C2', 'cs1Label': 'subcat', 'cs1': 'DNS_TUNNELING', 'msg': 'Malicious activity', 'dhost': 'bad.com'}
+    
+    """
     parsed_data = {}
-    key_list = ["cat", "cs1", "cs2Label", "cs2", "cs3Label", "cs3", "cs4Label", "cs4", "cn1Label", "cn1", "msg", "dhost", "dst"]
+    key_list = ["cat", "cs1", "cs2Label","cs1Label", "cs2", "cs3Label", "cs3", "cs4Label", "cs4", "cn1Label", "cn1", "msg", "dhost", "dst"]
+    
     for key in key_list:
         parsed_data[key] = parse_key_value(input_string,key)
     return parsed_data
 
 def parse_key_value(input_string,key, end=" "):
-    if key == "msg":
-        start_index = input_string.find(key + "=")
-        value = input_string[start_index + len(key) + 1:].split('.')[0]
-        return ''.join(value.split(end, 1))
+    """
+    Parse a specific key-value pair from an input string.
+
+    Parameters:
+    - input_string (str): The input string containing key-value pairs.
+    - key (str): The key to extract from the input string.
+    - end (str, optional): The delimiter indicating the end of the value. Default is a space.
+
+    Returns:
+    - str or int : The parsed value corresponding to the given key.
+
+    Example:
+    input_str = "msg=Malicious activity was reported. dhost=bad.com"
+    parse_key_value(input_str, "msg")
+    'Malicious activity was reported'
+    parse_key_value(input_str, "dhost")
+    'bad.com'
     
+    """
+    if key == "msg":
+        initial_index = input_string.find(key  + "=")
+        value = input_string[initial_index + len(key) + 1:].split('.')[0]
+        return ' '.join(value.split(end, 1))
+    
+    elif key+'=' in input_string:
+        initial_index = system_log_string.find(key+"=")
+        final_index = system_log_string[initial_index:].find(end)
+        final_string = system_log_string[initial_index:final_index+initial_index]
+        return final_string.split("=",1)[1]
     else:
-        start_index = input_string.find(key + "=")
-        end_index = input_string[start_index:].find(end)
-        field = input_string[start_index:start_index + end_index]
-        value = field.split("=", 1)[1]
-        return  int(value) if value.isdigit() else value
+        return 'None'
 
 if __name__ == "__main__":
     system_log_string = "SAC:0|Sacumen|CAAS|2021.2.0|3|MALICIOUS|High|cat=C2 cs1Label=subcat cs1=DNS_TUNNELING cs2Label=vueUrls cs2=https://aws-dev.sacdev.io/alerts?filter=alertId%3D%3D81650 cs3Label=Tags cs3=USA,Finance cs4Label=Url cs4=https://aws-dev.sacdev.io/settings/tir?rules.sort=4%3A1&filter=state%3D%3D2&selected=9739323 cn1Label=severityScore cn1=900 msg=Malicious activity was reported in CAAS\= A threat intelligence rule has been automatically created in DAAS. dhost=bad.com dst=1.1.1.1"
-    system_log_string += ' '
-    parsed_data = parsed_string(system_log_string)
+    system_log_string+=' '
+    parsed_data = processed_string(system_log_string)
     print(json.dumps(parsed_data,indent=4))
--- test_log_parserer.py
+++ test_log_parserer.py
@@ -1,29 +1,40 @@
 import pytest
-from log_parserer import parsed_string
+from log_parserer import processed_string
 
-def test_parsed_string():
+def test_string_parserer():
+    """
+        Test function for the 'parsed_string' method in the log_parserer module.
+        It checks if the 'parsed_string' method correctly parses a given system log string and returns the expected result.
+        Example:
+        >>> test_parsed_string()
+    """
+    # Input system log string
     input_string = "SAC:0|Sacumen|CAAS|2021.2.0|3|MALICIOUS|High|cat=C2 cs1Label=subcat cs1=DNS_TUNNELING cs2Label=vueUrls cs2=https://aws-dev.sacdev.io/alerts?filter=alertId%3D%3D81650 cs3Label=Tags cs3=USA,Finance cs4Label=Url cs4=https://aws-dev.sacdev.io/settings/tir?rules.sort=4%3A1&filter=state%3D%3D2&selected=9739323 cn1Label=severityScore cn1=900 msg=Malicious activity was reported in CAAS\= A threat intelligence rule has been automatically created in DAAS. dhost=bad.com dst=1.1.1.1"
     input_string += ' '
-
-    expected_result = {
+    
+    # Expected result after parsing
+    expected_output = {
                 "cat": "C2",
                 "cs1": "DNS_TUNNELING",
                 "cs2Label": "vueUrls",
+                "cs1Label": "subcat",
                 "cs2": "https://aws-dev.sacdev.io/alerts?filter=alertId%3D%3D81650",
                 "cs3Label": "Tags",
                 "cs3": "USA,Finance",
                 "cs4Label": "Url",
                 "cs4": "https://aws-dev.sacdev.io/settings/tir?rules.sort=4%3A1&filter=state%3D%3D2&selected=9739323",
                 "cn1Label": "severityScore",
-                "cn1": 900,
-                "msg": "Maliciousactivity was reported in CAAS\\= A threat intelligence rule has been automatically created in DAAS",
+                "cn1": "900",
+                "msg": "Malicious activity was reported in CAAS\\= A threat intelligence rule has been automatically ally created in DAAS",
                 "dhost": "bad.com",
                 "dst": "1.1.1.1"
             }
     
-    result = parsed_string(input_string)
-   
-    assert result == expected_result
+    #parsed string
+    result = processed_string(input_string)
+    
+    # Assertion to check if the result matches the expected result
+    assert result == expected_output
 
 if __name__ == "__main__":
     pytest.main()
